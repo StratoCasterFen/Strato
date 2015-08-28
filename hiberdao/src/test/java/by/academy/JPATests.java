a888caa409db0bfd0427df1fee3ea728aa9bb545
Pavel Matsak <mpa@tut.by>
2015-08-28 11:25:18 +0300

    test

--- hiberdao/src/test/java/by/academy/JPATests.java
+++ hiberdao/src/test/java/by/academy/JPATests.java
@@ -1,6 +1,11 @@
 package by.academy;
 
+//import static org.junit.Assert.assertEquals;
 import static junit.framework.Assert.assertEquals;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,46 +26,45 @@ import by.academy.pojos.User;
 
 
 public class JPATests {
-	private static Logger logger = LogManager.getLogger(JPATests.class.getName());
+	//private static Logger logger = LogManager.getLogger(JPATests.class.getName());
     EntityManagerFactory emf;
     EntityManager em;
     
     @Before
     public void init() {
-    	logger.info("init");
+    	//logger.info("init");
     	try{
-    	 logger.info(emf);	
+    	 //logger.info(emf);	
         emf = Persistence.createEntityManagerFactory("hiberpolice");
        
     	}catch (Exception e){
-    		 logger.error(e);
+    	//	 logger.error(e);
     	}
         em = emf.createEntityManager();
     }
     
     @Test
-    public void saveTest() {
-		logger.info("saveTest");
+    public void saveTest() throws Exception {
+	//	logger.info("saveTest");
 		persistSimplePOJO();
 		try {
-//			List results = em.createQuery("SELECT COUNT(id) FROM hiberpolice.users id")
-//					.getResultList();
-//			long size = results.isEmpty() ? 0 : (Long) results.get(0);
-		//	assertEquals(1L, size);
+			List results = em.createQuery("SELECT COUNT(u) FROM User u").getResultList();
+			long size = results.isEmpty() ? 0 : (Long) results.get(0);
+			org.junit.Assert.assertEquals(6l, size);
 		} catch (Exception e) {
-			logger.error("cant to perform result",e);
+	//		logger.error("cant to perform result",e);
 		}
 
 	}
     
-    @Test
-    public void manyToManyTest() {
-        persistSimplePOJO();
-        logger.info("saveTest");
-        User user = em.find(User.class, 1l);
-        logger.info(user.getUser_role().size());
-        assertEquals(2, user.getUser_role().size());
-    }
+//    @Test
+//    public void manyToManyTest() {
+//        persistSimplePOJO();
+//    //    logger.info("saveTest");
+//        User user = em.find(User.class, 1l);
+//      //  logger.info(user.getUser_role().size());
+//        assertEquals(2, user.getUser_role().size());
+//    }
  
     
     @After
@@ -70,17 +74,17 @@ public class JPATests {
     }
     
     public void persistSimplePOJO() {
-    	logger.info("persistSimplePOJO");
+  //  	logger.info("persistSimplePOJO");
         EntityTransaction tx = em.getTransaction();
         tx.begin();
-    	logger.info("persist");
+    //	logger.info("persist");
     	User user=getUser();
         em.persist(user);
         tx.commit();
     }
     
     public User getUser() {
-    	logger.info("getUser");
+    //	logger.info("getUser");
         User user = new User();
         user.setUserName("Dgordano");
         user.setPassword("fhjfhfhg4df4g654dfg65");
@@ -90,7 +94,7 @@ public class JPATests {
         role.add(new Role("Guest"));
 
         user.setUser_role(role);
-        logger.info("get "+user);
+ //       logger.info("get "+user);
         return user;
     }
 }
